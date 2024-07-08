75b26a64b33d0a70f95faea30d57208c4bfaf35f
Vishal <83269653+overlordvishal97@users.noreply.github.com>
2024-07-08 23:43:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+
+my_lat = 15.828126
+my_long = 78.037277
+
+parameters = {"lat":my_lat,"lng":my_long, "formatted" : 0}
+
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+now = datetime.now()
+
+print(now.hour)
